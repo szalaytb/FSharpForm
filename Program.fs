1998e3e5aff6ccfbef79265a24b385f364f87855
szalaytb <57213660+szalaytb@users.noreply.github.com>
2020-01-23 07:02:43 +0100

    Added regexp example

--- Program.fs
+++ Program.fs
@@ -7,9 +7,16 @@ open System.Drawing
 
 open System.Windows.Forms
 
+open System.Text.RegularExpressions
+
+//[<Literal>]
+//let connectionString = "Data Source=localhost; Initial Catalog=SqlInFsharp; Integrated Security=True;"
+
 let form = new Form(Text = "First F# form")
 let button1 = new Button(Text = "Click me to close!", Dock = DockStyle.Top)
 let button2 = new Button(Text = "This button does something", Dock = DockStyle.Top)
+let button3 = new Button(Text = "Regex button", Dock = DockStyle.Top)
+let textbox1 = new TextBox (Text = "Sample text",Dock = DockStyle.Bottom)
 let label = new Label(Text = "This is a test application", Dock = DockStyle.Bottom)
 
 button1.Click.Add(fun _ -> Application.Exit() |> ignore)
@@ -20,12 +27,18 @@ let rnd = new Random()
 let r, g, b = rnd.Next(256), rnd.Next(256), rnd.Next(256)
 form.BackColor <- Color.FromArgb(r, g, b) )
 
+button3.Click.Add(fun _ ->
+   let r = Regex.IsMatch("a","^a*$")
+   if r then textbox1.Text <- "true" else textbox1.Text <- "false"
+)
 //form.Controls.Add(button1)
 //form.Controls.Add(button2)
 //form.Controls.Add(label)
 
 form.Controls.AddRange([|button1 :> Control;
 button2 :> Control;
+button3 :> Control;
+textbox1 :> Control;
 label :> Control|]);
 
 form.Show()
